e21dd6c53e48d05b5e045c56ac7f8ca5e19df647
Wade Friesen <wade@wadefriesen.com>
2016-08-10 15:03:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <App ip='http://ip-api.com/json' weather='http://api.openweathermap.org/data/2.5/weather?appid=b2a6681590bccccae332ee769dd2826b&units=metric&lat=' credit="http://codepen.io/joshbader/pen/EjXgqr" />,
+  <App ip='http://ip-api.com/json' weather='http://api.openweathermap.org/data/2.5/weather?appid=/*insert api key*/&units=metric&lat=' credit="http://codepen.io/joshbader/pen/EjXgqr" />,
   document.getElementById('root')
 );
